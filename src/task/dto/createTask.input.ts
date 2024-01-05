da3cc368758ed7505fc83175d06a668cdf69c8fc
Onřej Znojil <ondrejznojil@gmail.com>
2024-01-05 01:29:43 +0100

    Lint fix

--- src/task/dto/createTask.input.ts
+++ src/task/dto/createTask.input.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsEmail, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class CreateTaskInput {
   @IsNotEmpty()
